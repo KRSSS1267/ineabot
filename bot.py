919da0260e9c23afc7c52d40d135753831813b41
KRSSS1267 <65147175+KRSSS1267@users.noreply.github.com>
2020-12-19 03:34:57 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,42 @@
-import discord
+mport discord,asyncio,youtube_dl
 from discord.ext import commands
 import os
+from dotenv import load_dotenv
 
-client = commands.Bot(command_prefix = ';')
+load_dotenv()
 
-@client.event
+
+
+
+def get_prefix(bot, msg):
+    """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
+
+    prefixes = [';'] #Your bot prefix(s)
+
+    return commands.when_mentioned_or(*prefixes)(bot, msg)
+
+bot=commands.Bot(command_prefix=get_prefix,description='Team Inea')
+
+
+
+
+exts=['music'] #Add your Cog extensions here
+
+
+@bot.event
 async def on_ready():
-
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
-
-  await client.change_presence(activity=discord.Game(name=";도움 | Team Inea"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", ur'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
-  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+    song_name='TWICE - What is love?' 
+    await client.change_presence(status=discord.Status.online)
+    await client.change_presence(activity=discord.Game(name=";도움 | Team Inea"))
+    print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 
-client.run(os.environ['token'])
+
+
+
+
+for i in exts:
+    bot.load_extension(i)
+
+
+bot.run(os.environ['TOKEN'])
