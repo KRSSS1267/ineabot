7313eed9cd8b4c0ebf505ffc98b62e70272eb8f1
KRSSS1267 <65147175+KRSSS1267@users.noreply.github.com>
2020-12-19 01:15:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ async def on_ready():
   # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
 
-  await client.change_presence(activity=discord.Streaming(name="!도움 | Team Inea", url='https://www.twitch.tv/ssstwitchtv'))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  await client.change_presence(activity=discord.Activity(name="!도움 | Team Inea", url='https://www.teaminea.xyz/'))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", ur'))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
   #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
   
