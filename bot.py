eb53bd94648b8b4f1b532ab3ca4d7555539e818b
KRSSS1267 <65147175+KRSSS1267@users.noreply.github.com>
2020-12-19 03:37:18 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ def get_prefix(bot, msg):
 
     return commands.when_mentioned_or(*prefixes)(bot, msg)
 
-bot=commands.Bot(command_prefix=get_prefix,description='Team Inea')
+bot=commands.Bot(command_prefix=get_prefix,description='Multipurpose Discord Bot')
 
 
 
